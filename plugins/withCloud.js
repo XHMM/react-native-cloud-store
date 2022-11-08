1474aa9b3ba30d3442dc15a9312840ee3f5f6f4a
xiao xin <super.xhmm@gmail.com>
2022-11-08 18:59:27 +0800

    fix: expo plugin not work correctly

--- plugins/withCloud.js
+++ plugins/withCloud.js
@@ -25,14 +25,14 @@ const withCloudPlugin = (config, options) => {
       if(documents.length) {
         documents.filter(doc => doc.enabled).map(doc => {
           const id = doc.containerId;
-          if (id) {
-            if (!id.startsWith('iCloud')) {
-              // https://developer.apple.com/documentation/uikit/documents_data_and_pasteboard/synchronizing_documents_in_the_icloud_environment#:~:text=An%20iCloud%20container%20identifier%20is%20case%2Dsensitive%20and%20must%20begin%20with%20%E2%80%9CiCloud.%E2%80%9D.
-              throw new Error("containerId must start with 'iCloud.'");
-            }
-            e['com.apple.developer.icloud-container-identifiers'].push(id);
-            e['com.apple.developer.ubiquity-container-identifiers'].push(id);
+          if(!id) return
+          if (!id.startsWith('iCloud')) {
+            // https://developer.apple.com/documentation/uikit/documents_data_and_pasteboard/synchronizing_documents_in_the_icloud_environment#:~:text=An%20iCloud%20container%20identifier%20is%20case%2Dsensitive%20and%20must%20begin%20with%20%E2%80%9CiCloud.%E2%80%9D.
+            throw new Error("containerId must start with 'iCloud.'");
           }
+
+          e['com.apple.developer.icloud-container-identifiers'].push(id);
+          e['com.apple.developer.ubiquity-container-identifiers'].push(id);
         })
       }
 
@@ -45,16 +45,18 @@ const withCloudPlugin = (config, options) => {
 
       documents.map(doc => {
         const id = doc.containerId;
-        if (id && doc.visible) {
-          const visibleName = doc.visibleName;
-          if (!visibleName)
-            throw new Error(`you need apply a visibleName config for ${id}`);
-          e.NSUbiquitousContainers[id] = {
-            NSUbiquitousContainerIsDocumentScopePublic: true,
-            NSUbiquitousContainerName: visibleName,
-            NSUbiquitousContainerSupportedFolderLevels: 'Any',
-          };
-        }
+        if(!id) return
+        if(!doc.enabled) return
+        if(!doc.visible) return
+
+        const visibleName = doc.visibleName;
+        if (!visibleName)
+          throw new Error(`you need apply a visibleName for ${id}`);
+        e.NSUbiquitousContainers[id] = {
+          NSUbiquitousContainerIsDocumentScopePublic: true,
+          NSUbiquitousContainerName: visibleName,
+          NSUbiquitousContainerSupportedFolderLevels: 'Any',
+        };
       })
 
       return config;
